5b31fbbf912a4951c45a184ab816df5bf5fa57da
zwlaka <773840180@qq.com>
2023-07-02 22:10:20 +0800

    update

--- lib/analysis/call_graph/call_graph.ml
+++ lib/analysis/call_graph/call_graph.ml
@@ -60,10 +60,9 @@ let make (wasm_mod : Wasm_module.t) : t =
 
 (** Convert call graph to its dot representation *)
 let to_dot (cg : t)( wasm_mod : Wasm_module.t) : string =
-  Printf.sprintf "digraph \"Call graph\" {\n%s\n%s\n}"
   let name = Wasm_module.get_funcname_1 wasm_mod n in
-  print_string name
-  
+  Printf.printf "%s\n" name;
+  Printf.sprintf "digraph \"Call graph\" {\n%s\n%s\n}"
     (String.concat ~sep:"\n"
        (List.map (Int32Set.to_list cg.nodes) ~f:(fun n ->
             Printf.sprintf "node%s [shape=record, mlabel=\"{%s}\"];" (Int32.to_string n) (name))))
